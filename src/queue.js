fb27b8318c8efd51fdd49f04a4337bc14d8c28c2
AlexKaroh <steam.alexkaroh@gmail.com>
2022-10-17 14:13:19 +0400

    feat: solve second task (2/3)

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.head = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+}
+class Queue {
+  constructor (){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  getUnderlyingList() {
+    let current = this.head;
+    while(current) {
+      current = current.next;
+    }
+  }
+
+  enqueue(value) {
+    let node = new Node(value);
+    if (this.head){
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return current.value;
   }
+
+
 }
 
 module.exports = {
